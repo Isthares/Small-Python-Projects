c97d13d3245214c338b1ecf63ee196ee9231e572
Esther Heralta Espinosa <47342634+Isthares@users.noreply.github.com>
2019-04-08 22:42:22 -0700

    Update specialPythagoreanTriplet.py

--- Special-Pythagorean-Triplet/specialPythagoreanTriplet.py
+++ Special-Pythagorean-Triplet/specialPythagoreanTriplet.py
@@ -2,9 +2,9 @@
 # Author: Esther Heralta Espinosa
 # Date: 04/06/19
 # Description of the program: A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a^2 + b^2 = c^2
-#							  For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
-#							  There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-#							  Find the product abc.
+#			      For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+#			      There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#			      Find the product abc.
 
 SUM = 1000
 
